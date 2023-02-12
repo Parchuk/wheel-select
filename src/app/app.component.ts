86f16749ffe6e3e346ba310489a8100066b1144e
sparchuk <sparchuk@itsmartflex.com>
2023-02-12 02:42:02 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-2';
+
+  items: string[] = ['dd', 'ee', 'ww'];
+ public onSelectionChanged(e: Event) {
+    console.log('eee', e)
+  }
+
+  data = [
+    {
+      list: ['sdfsadfasf1', '2ddddfd', '3dfsdf', '4sdfds', '5sdfsd', '6sdfdsf', '7fffdfd', '8sfdf', '9dsfdsf'],
+      currentIndex: 2
+    }
+  ]
+
+  change ({ gIndex, iIndex }: any) {
+    // console.log(gIndex, iIndex)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {DataPickerModule} from './data-picker/data-picker.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
